172d606497f5ff9976146dd62ddacb13f81d5334
Serine99 <serine.sardaryan1@gmail.com>
2022-09-03 14:50:04 +0400

    Create User

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,1 +1,35 @@
 import Userdb from "../model/model.js";
+
+// create and save new user
+
+class usersCtrl {
+	static async userCreate(req, res) {
+		if (!req.body) {
+			res.status(400).send({ message: "Content can not be empty" });
+			return;
+		}
+
+		const user = new Userdb({
+			name: req.body.name,
+			email: req.body.email,
+			gender: req.body.gender,
+			status: req.body.status,
+		});
+		user
+			.save(user)
+			.then((data) => {
+				res.send(data);
+			})
+			.catch((err) => {
+				res.status(500).send({
+					message: err.message || "Some error occurred during user creation",
+				});
+			});
+	}
+
+	static async userFind(req, res) {}
+	static async userUpdate(req, res) {}
+	static async userDelete(req, res) {}
+}
+
+export default usersCtrl;
--- server/routes/router.js
+++ server/routes/router.js
@@ -1,10 +1,16 @@
 import express from "express";
 import { homeRoutes, add_user, update_user } from "../services/render.js";
+import usersCtrl from "../controller/controller.js";
 const route = express.Router();
 
 route.get("/", homeRoutes);
 route.get("/add-user", add_user);
 
 route.get("/update-user", update_user);
+
+route.post("api/users", usersCtrl.userCreate);
+route.get("api/users", usersCtrl.userFind);
+route.put("api/users/:id", usersCtrl.userUpdate);
+route.delete("api/users/:id", usersCtrl.userDelete);
 
 export default route;
