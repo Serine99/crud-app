2e81ca4f98d9f3556d28fca035976a9aecd18350
Serine99 <serine.sardaryan1@gmail.com>
2022-09-07 18:15:28 +0400

    Update user

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,6 +6,35 @@ $("#update_user").submit(function (event) {
 	let unindexed_array = $(this).serializeArray();
 
 	let data = {};
-	$.map(unindexed_array, function (n, i) {});
+	$.map(unindexed_array, function (n, i) {
+		data[n["name"]] = n["value"];
+	});
 	console.log(unindexed_array);
+
+	let request = {
+		url: `http://localhost:3000/api/users/${data.id}`,
+		method: "PUT",
+		data: data,
+	};
+	$.ajax(request).done(function (response) {
+		alert("Data updated successfully");
+	});
 });
+
+if (window.location.pathname == "/") {
+	$ondelete = $(".table tbody td a.delete");
+	$ondelete.click(function () {
+		let id = $(this).attr("data-id");
+
+		let request = {
+			url: `http://localhost:3000/api/users/${id}`,
+			method: "DELETE",
+		};
+		if (confirm("Do you really want to delete this record")) {
+			$.ajax(request).done(function (response) {
+				alert("Data deleted successfully");
+				location.reload();
+			});
+		}
+	});
+}
